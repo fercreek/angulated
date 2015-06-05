c771a4b4564e469dc898f83a2de395cff05486b0
Fernando Contreras <fercreek@gmail.com>
2015-06-05 01:20:59 -0500

    Table added and initial inventory, demand and sales done.

--- appController.js
+++ appController.js
@@ -1,7 +1,34 @@
 app.controller("appController", appController);
 
-appController.$inject = [];
+appController.$inject = ["$scope"];
 
-function appController(){
-    console.log("hola");
+function appController($scope){
+    // $scope.demand = "";
+
+    $scope.changeValue = function(){
+        var provider = $scope.provider;
+        var demand = $scope.demand = $scope.demandFn();
+        var sales = $scope.sales = $scope.salesFn();
+        // var x = 1;
+        // $scope.demand = x + 1 ;
+
+        // $scope.demand = (-100 * Math.log(1-Math.random()));
+    };
+
+    $scope.demandFn = function(){
+        return (-100 * Math.log(1-Math.random()));
+    };
+
+    $scope.salesFn = function(){
+        if ($scope.demand > $scope.provider){
+            return $scope.demand;
+        } else {
+            return $scope.provider;
+        }
+    };
+
+    $scope.sales = function(){
+
+    };
+
 }
